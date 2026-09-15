2d7057bdfc735c481a9a11d8e31914ab352a2d86
WolffLoney <luccafelipe99@gmail.com>
2026-09-14 21:41:39 -0300

    fim da aula..realizar correções

---
+++ Dev.AppMobile08/src/pages/Home.js
@@ -0,0 +1,112 @@
+import { Button, StyleSheet, Text, TextInput, View } from "react-native"
+import { useState } from "react";
+
+const dadosForm = {
+    nome: '',
+    idade: '',
+    senha: '',
+    maisInfos: ''
+}
+
+export default function Home(){
+    const [nomeestado, setNomeestado] = useState('');
+
+    function aoDigitar(textoDigitado){
+        console.log(`Texto digitado: ${textoDigitado}`);
+    }
+
+    function aoEnviar(){
+        //destructuring do objetopikama
+        const {nome, idade, senha, maisInfos} = dadosForm;
+        console.log('Formulário enviado!: ', {
+            nome,
+            idade,
+            senha: '*'. repeat(senha.length),
+            maisInfos
+        });
+        //chamada de backend passando 'dadosForm'
+    }
+
+    return(
+        <View style={estilo.container}>
+            <Text style={estilo.textao}>
+
+            </Text>
+            <View style={estilo.containerinput}>
+                <Text style={estilo.label}> Nome: </Text>
+                <TextInput
+                    placeholder="Digite aqui o seu texto"
+                    style={estilo.input}
+                    onChangeText={setNomeestado}
+                    value={nomeestado}
+                />
+            </View>
+            <View style={estilo.containerinput}>
+                <Text style={estilo.label}> Idade: </Text>
+                <TextInput
+                    placeholder="Digite aqui o seu texto"
+                    style={estilo.input}
+                    onChangeText={(texto) => (dadosForm.idade = texto)}
+                    keyboardType="numeric"
+                />
+            </View>
+             <View style={estilo.containerinput}>
+                <Text style={estilo.label}> Senha: </Text>
+                <TextInput
+                    placeholder="Digite aqui o seu texto"
+                    style={estilo.input}
+                    onChangeText={(texto) => (dadosForm.senha = texto)}
+                    secureTextEntry
+                />
+            </View>
+
+             <View style={estilo.containerinput}>
+                <Text style={estilo.label}> Fale mais sobre você: </Text>
+                <TextInput
+                    style={estilo.input}
+                    placeholder="Conte mais sobre você"
+                    onChangeText={(texto) => (dadosForm.maisInfos = texto)}
+                    multiline
+                    maxLength={60}
+                />
+            </View>
+            <Button
+                title="Enviar"
+                color="#118ab2"
+                onPress={aoEnviar}
+                />
+
+        </View>
+    )
+}
+
+const estilo = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+        backgroundColor: '#dadada',
+        flexDirection: 'column',
+    },
+    containerinput:{
+        flexDirection: 'row',
+        gap: 8,
+        alignItems: 'center',
+        marginBottom: 12,
+        marginStart: 8
+    },
+    input:{
+        borderWidth: 1,
+        borderColor: '#000',
+        fontSize: 16,
+        paddingHorizontal: 12, 
+        paddingVertical: 10,
+        backgroundColor: '#fff'
+    },
+    label: {
+        fontSize: 14,
+        fontWeight: 'bold',
+        gap: 8,
+    },
+
+})
